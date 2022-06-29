bc5d8e47bcb43555e7b99e2876fa719026265493
nekusu <nekusu.dev@gmail.com>
2022-06-29 03:19:26 -0300

    feat: create basic structure of the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,38 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    outline: none;
+    border: none;
+    text-decoration: none;
+  }
+`;
+
+const StyledApp = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  transition: background-color 0.25s;
+`;
+
+const Content = styled.div`
+  width: 100%;
+  max-width: 1000px;
+  padding: 32px;
+`;
+
 function App() {
   return (
-    <div className="App"></div>
+    <>
+      <GlobalStyle />
+      <StyledApp>
+        <Content />
+      </StyledApp>
+    </>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <HashRouter>
+    <Routes>
+      <Route path="/*" element={<App />} />
+    </Routes>
+  </HashRouter>,
 );
