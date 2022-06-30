f56ddf7e29689c4ca478ed7dbc8c19780f88c0b2
nekusu <nekusu.dev@gmail.com>
2022-06-30 19:09:41 -0300

    feat: create Loading component

---
+++ src/components/Loading/Loading.styles.tsx
@@ -0,0 +1,34 @@
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
+import { CircularProgress } from 'react-cssfx-loading';
+
+const Container = styled(motion.div).attrs(() => ({
+  initial: { opacity: 0 },
+  animate: { opacity: 1 },
+  exit: { opacity: 0 },
+}))`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+`;
+
+const Loading = styled(CircularProgress).attrs((props) => ({
+  color: props.theme.main,
+}))`
+  height: 100%;
+  max-height: 80px;
+  width: 100%;
+  max-width: 80px;
+
+  circle {
+    transition: stroke 0.25s;
+  }
+`;
+
+const Styled = {
+  Container,
+  Loading,
+};
+
+export default Styled;
---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,11 @@
+import Styled from './Loading.styles';
+
+function Loading() {
+  return (
+    <Styled.Container>
+      <Styled.Loading />
+    </Styled.Container>
+  );
+}
+
+export default Loading;
