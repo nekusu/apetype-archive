5161b79a401595378440bf74b1d8fb8b5879e637
nekusu <nekusu.dev@gmail.com>
2022-07-01 18:47:32 -0300

    feat: add test restart and random theme loader

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,25 +1,53 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { AnimatePresence } from 'framer-motion';
+import uniqid from 'uniqid';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
-import { setWords } from '../../components/TypingTest/TypingTest.slice';
 import { TestStats, TypingTest } from '../../components';
+import { setTheme } from '../../app/config.slice';
+import themes from '../../themes/_list';
 import Styled from './Home.styles';
 
 function Home() {
   const dispatch = useAppDispatch();
-  const { mode, time } = useAppSelector(({ config }) => config);
+  const { randomTheme, mode, time } = useAppSelector(({ config }) => config);
+  const { seconds } = useAppSelector(({ testStats }) => testStats);
+  const [testId, setTestId] = useState(uniqid());
+  const chooseRandomTheme = useCallback(async () => {
+    if (randomTheme === 'off') return;
+    let filteredThemes = themes;
+    if (randomTheme === 'light' || randomTheme === 'dark') {
+      filteredThemes = themes.filter((theme) => theme.mode === randomTheme);
+    }
+    const newTheme = filteredThemes[Math.floor(Math.random() * filteredThemes.length)];
+    dispatch(setTheme((await import(`../../themes/${newTheme.name}.ts`)).default));
+  }, [dispatch, randomTheme]);
+  const restartTest = useCallback(() => {
+    setTestId(uniqid());
+    chooseRandomTheme();
+  }, [chooseRandomTheme]);
+  const handleKeyDown = useCallback((e: KeyboardEvent) => {
+    if (e.key === 'Tab') {
+      e.preventDefault();
+      restartTest();
+    }
+  }, [restartTest]);
 
   useEffect(() => {
-    return () => {
-      dispatch(setWords([]));
-    };
-  }, [dispatch]);
+    window.addEventListener('keydown', handleKeyDown);
+    chooseRandomTheme();
+    return () => window.removeEventListener('keydown', handleKeyDown);
+  }, [chooseRandomTheme, handleKeyDown]);
+  useEffect(() => {
+    if (!seconds) {
+      restartTest();
+    }
+  }, [restartTest, seconds]);
 
   return (
     <Styled.Home>
       <TestStats />
       <AnimatePresence exitBeforeEnter>
-        <TypingTest key={`${mode}-${time}`} />
+        <TypingTest key={`${mode}-${time}-${testId}`} />
       </AnimatePresence>
     </Styled.Home>
   );
