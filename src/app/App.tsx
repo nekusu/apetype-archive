385c26ab143cafb2998c690b5300cc369f54dc0b
nekusu <nekusu.dev@gmail.com>
2022-07-12 08:13:59 -0300

    refactor: move store provider to index

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,34 +1,30 @@
-import { Provider } from 'react-redux';
 import { Route, Routes, useLocation } from 'react-router-dom';
 import { AnimatePresence, domAnimation, LazyMotion, MotionConfig } from 'framer-motion';
 import { Header, ThemeProvider } from '../components';
 import { Home } from '../pages';
-import store from './store';
 import Styled, { GlobalStyle } from './App.styles';
 
 function App() {
   const location = useLocation();
 
   return (
-    <Provider store={store}>
-      <ThemeProvider>
-        <LazyMotion features={domAnimation}>
-          <MotionConfig transition={{ opacity: { duration: 0.25 } }}>
-            <GlobalStyle />
-            <Styled.App>
-              <Styled.Content>
-                <Header />
-                <AnimatePresence exitBeforeEnter>
-                  <Routes location={location} key={location.pathname}>
-                    <Route path="/" element={<Home />} />
-                  </Routes>
-                </AnimatePresence>
-              </Styled.Content>
-            </Styled.App>
-          </MotionConfig>
-        </LazyMotion>
-      </ThemeProvider>
-    </Provider>
+    <ThemeProvider>
+      <LazyMotion features={domAnimation}>
+        <MotionConfig transition={{ opacity: { duration: 0.25 } }}>
+          <GlobalStyle />
+          <Styled.App>
+            <Styled.Content>
+              <Header />
+              <AnimatePresence exitBeforeEnter>
+                <Routes location={location} key={location.pathname}>
+                  <Route path="/" element={<Home />} />
+                </Routes>
+              </AnimatePresence>
+            </Styled.Content>
+          </Styled.App>
+        </MotionConfig>
+      </LazyMotion>
+    </ThemeProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import ReactDOM from 'react-dom/client';
 import { HashRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './app/App';
+import store from './app/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
-  <HashRouter>
-    <Routes>
-      <Route path="/*" element={<App />} />
-    </Routes>
-  </HashRouter>,
+  <Provider store={store}>
+    <HashRouter>
+      <Routes>
+        <Route path="/*" element={<App />} />
+      </Routes>
+    </HashRouter>
+  </Provider>,
 );
